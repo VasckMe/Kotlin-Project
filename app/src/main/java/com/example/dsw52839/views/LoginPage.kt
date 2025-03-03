5bd5c86827cb20abdd0a32cf78c05f57b5ac941b
Anton K <>
2025-03-03 20:42:35 +0100

    Updates on login page

--- app/src/main/java/com/example/dsw52839/views/LoginPage.kt
+++ app/src/main/java/com/example/dsw52839/views/LoginPage.kt
@@ -1,14 +1,19 @@
 package com.example.dsw52839.views
 
+import android.graphics.Paint.Align
 import android.graphics.drawable.Icon
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.CircleShape
@@ -19,10 +24,13 @@ import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -32,25 +40,56 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.dsw52839.R
 import com.example.dsw52839.ui.theme.PurpleBorder
+import com.example.dsw52839.ui.theme.PurpleButton
 import com.example.dsw52839.ui.theme.PurpleTextAndIcon
 import com.example.dsw52839.utils.Routes
 
+class LoginViewModel: ViewModel() {
+    val text = ""
+}
+
 @Composable
-fun LoginPage(navController: NavController) {
+fun LoginPage(navController: NavController, viewModel: LoginViewModel = LoginViewModel()) {
+
     Column(
-        Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
+        Modifier
+            .fillMaxSize()
+            .padding(horizontal = 20.dp),
         verticalArrangement = Arrangement.Center,
     ) {
-        Text(text = "Sign In", color = PurpleTextAndIcon)
+
+        Image(
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+                .size(130.dp),
+            painter = painterResource(id =  R.drawable.logo),
+            contentDescription = "App Logo"
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(text = "Sign In", color = PurpleTextAndIcon,
+            fontWeight = FontWeight.W700,
+            fontSize = 30.sp,
+            letterSpacing = 0.sp,
+            textAlign = TextAlign.Left
+        )
 
         Spacer(modifier = Modifier.height(40.dp))
 
@@ -62,8 +101,30 @@ fun LoginPage(navController: NavController) {
 
         Spacer(modifier = Modifier.height(40.dp))
 
-        Button(onClick = { navController.navigate(route = Routes.registerPage) }) {
-            Text(text = "Go To Sign Up")
+        TextButton(
+            modifier = Modifier
+                .align(alignment = Alignment.End),
+            onClick = { navController.navigate(route = Routes.registerPage) }
+
+        ) {
+            Text(
+                "Forget password",
+                color = PurpleTextAndIcon,
+                fontWeight = FontWeight.W700,
+                fontSize = 15.sp,
+                letterSpacing = 0.sp,
+            )
+        }
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Button(
+            colors = ButtonDefaults.buttonColors(containerColor = PurpleButton),
+            modifier = Modifier.fillMaxWidth(),
+            onClick = { navController.navigate(route = Routes.registerPage) }
+        )
+        {
+            Text(text = "Sign In")
         }
     }
 }
@@ -90,7 +151,6 @@ fun SimpleTextField(placeholder: String, leadingIcon: ImageVector, showTrailingI
         singleLine = true,
         isError = text.value.isNotEmpty() && text.value.length < 8,
         modifier = Modifier
-            .padding(horizontal = 20.dp)
             .fillMaxWidth()
             .clip(RoundedCornerShape(15.dp))
             .border(2.dp, PurpleBorder, RoundedCornerShape(15.dp))
